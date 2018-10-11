8c5c617c53dc7e98c03517ce6575657ba681ccc0
L-WJ <louwangjun_rf@foxmail.com>
2018-10-11 23:01:24 +0800

    first

---
+++ public/javascripts/content.js
@@ -0,0 +1,361 @@
+let shade_status
+let submit_content_box_status
+let contentID = window.location.href.split("/").pop()
+let comment_comment_box
+
+arrows.onclick = () => {
+  shade.style.display = "block"
+  setTimeout(() => shade.style["background-color"] = "rgba(0,0,0,0.5)" ,0)
+  arrows.style.display = "none"
+  arrows.parentNode.classList.add("trans-in")
+}
+
+
+$('.btn_comments_Comments').click(function(){
+  $('.submit_content_box').css({'z-index':'0'})
+  shade.style.display = "block"
+  submit_content_box_status = true
+  setTimeout(() => shade.style["background-color"] = "rgba(0,0,0,0.5)" ,0)
+  $(this).nextAll('.commentsComments').find('form').css({'height':'320px'})
+})
+
+
+
+$('.switch button').click(function(){
+  shade.style.display = "block"
+  $('.submit_content_box').css({'z-index':'0'})
+  setTimeout(() => shade.style["background-color"] = "rgba(0,0,0,0.5)" ,0)
+  let commentID = $(this).parents('.comment_box').find('span')[1].textContent
+  let el = $(this).parents('.comment_box').find('.comment_comment_box > ul:first-child')
+  shade.style.display = "block"
+  axios({
+    method:'get',
+    url: '/commentsComments/' + commentID ,
+  }).then((res) => {
+      res_status(res.data, el)
+  })
+})
+
+
+shade.onclick = () => {
+  if (shade_status) return
+  shade_status = true
+  setTimeout(() =>  shade.style["background-color"] = "transparent" ,0)
+  if (arrows.style.display === "none") {
+    arrows.parentNode.classList.add("trans-out")
+    setTimeout(() => {
+      arrows.parentNode.classList.remove("trans-in", "trans-out")
+      arrows.style.display = "block"
+    },600)
+  } else if (submit_content_box_status){
+      $('.commentsComments').find('form').css({'height':'0px'})
+      setTimeout(() => {
+        $('.submit_content_box').css({'z-index':'100'})
+        submit_content_box_status = false
+      },600)
+  } else {
+      comment_comment_box.removeClass('comment_comment_box_in')
+      setTimeout(() => {
+        comment_comment_box.css('display','none')
+        comment_comment_box.find('ul:nth-child(1) li').not('.clone').remove()
+        
+        comment_comment_box.find('.lookTalk_container_in li').not('.clone_lookTalk').remove()
+        comment_comment_box.find('.lookTalk_container_in').removeClass('lookTalk_container_in')
+        comment_comment_box.find('.return_in').css('display',"none")
+        comment_comment_box.find('.return_in').removeClass('return_in')
+
+        $('.submit_content_box').css({'z-index':'100'})
+        submit_content_box_status = false
+      },600)
+  }
+  setTimeout(() => {
+    shade.style.display = "none"
+    shade_status = false
+  },600)
+}
+
+
+$('.submit_content').click(function(e){
+  let els = $(this).parents('.comments').children('span')
+  let text = $(this).parents('form.form-horizontal').find('.submit_text')[0]
+  let event = e || window.event
+  if (event.preventDefault) event.preventDefault()
+  else event.returnValue = false
+  if(text.value === "" ) {
+    $(".modal-title span").text("错误！")
+    $(".modal-body span").text("评论内容不能为空！")
+    $(".modal-footer button").addClass("btn-warning").text("Close")
+    modal_status()
+  } else {
+      axios({
+        method:'post',
+        url:$(this).parents('form.form-horizontal')[0].action,
+        data:{
+          text: text.value,
+          contentID:contentID,
+          commentid:els[0] ? els[0].textContent - 0 : undefined,
+        }
+      }).then((res) => {
+          res_status(res.data, $(this).parents('.comment_box'))
+      })
+  }
+})
+
+
+
+
+
+
+
+function res_status(data, el) {
+  console.log(data)
+  switch(data.status) {
+    case 50: {
+      $(".modal-title span").text("删除成功")
+      $(".modal-body span").text("正在返回")
+      $(".modal-footer button").removeClass("btn-warning").addClass("btn-success").text("刷新中...")
+      $(".bs-example-modal-sm").modal("show")
+      setTimeout(() => window.location.href = "/", 100)
+      break
+    }
+
+    case 51: {
+      el.parents('.comment_box').remove()
+      break
+    }
+
+    case 52: {
+      el.parents('.comments').find('.sumComments').text(el.parents('.comments').find('.sumComments').text() - 1)
+      if (el.parents('.comments').find('.sumComments').text() - 0 === 0) {
+        el.parents('.comment_box').find('.switch').css('display','none')
+        shade.click()
+      }
+      el.parent().parent().remove()
+      break
+    }
+
+    case 53: {
+      if (data.status_ === 1) el.parent().parent().find('span').text(el.parent().parent().find('span').text() - 0 + 1)
+      else el.parent().parent().find('span').text(el.parent().parent().find('span').text() - 1)
+      break
+    }
+
+    case 100: {
+      $(".modal-title span").text("评论成功")
+      $(".modal-body span").text("正在刷新")
+      $(".modal-footer button").removeClass("btn-warning").addClass("btn-success").text("刷新中...")
+      $(".bs-example-modal-sm").modal("show")
+      setTimeout(() => window.location.href = "./" + data.contentID, 100)
+      break
+    }
+
+    case 101: {
+      shade.click()
+      el.find('.submit_text')[0].value = ''
+      el1 = el.find('.switch')[0]
+      el2 = el.find('.sumComments')[0]
+      $(".modal-title span").text("评论成功")
+      $(".modal-body span").text("即将关闭提示！")
+      $(".modal-footer button").removeClass("btn-warning").addClass("btn-success").text("Close")
+      $(".bs-example-modal-sm").modal("show")
+      modal_status()
+      el2.textContent = el2.textContent - 0 + 1
+      if (el1.style && el1.style.display === "none") el1.style.display = "block"
+      break
+    }
+
+    case 102: {
+      comment_comment_box = el.parent()
+      createComment_s(data, el)
+      break
+    }
+
+    case 108: {
+      $(".modal-title span").text("评论成功")
+      $(".modal-body span").text("即将关闭提示！")
+      $(".modal-footer button").removeClass("btn-warning").addClass("btn-success").text("Close")
+      $(".bs-example-modal-sm").modal("show")
+      modal_status()
+      data.srcoll = true
+      comment_comment_box = el
+      createComment_s(data, el.find('ul:nth-child(1)'))
+      break
+    }
+
+    case 109: {
+      createLookTales(data.data, el.parents('.comment_comment_box').find('ul:nth-child(2)'))
+      break
+    }
+  }
+  
+}
+
+
+function modal_status() {
+  $(".bs-example-modal-sm").modal("show")
+  let modal_ID = setTimeout(() => {
+    $(".bs-example-modal-sm").modal("hide")
+  }, 500)
+  $(".bs-example-modal-sm").on("hidden.bs.modal", () => {
+    clearTimeout(modal_ID)
+    $(".bs-example-modal-sm").off("hidden.bs.modal")
+  })
+}
+
+$("textarea").on('keydown',function(e) {
+  if (e.keyCode == 9) {
+    e.preventDefault()
+    let indent = '    '
+    let start = this.selectionStart
+    let end = this.selectionEnd
+    let selected = window.getSelection().toString();
+    selected = indent + selected.replace(/\n/g, '\n' + indent)
+    this.value = this.value.substring(0, start) + selected + this.value.substring(end)
+    this.setSelectionRange(start + indent.length, start+ selected.length)
+  }
+})
+
+function createComment_s(data, el) {
+  if(!Array.isArray(data.commentCommentsData)) data.commentCommentsData = [data.commentCommentsData] 
+  data.commentCommentsData.forEach((item) => {
+    if (item.tousername) $('.clone').find('div.reply_tip .lookTalk').css('display','block')
+    else $('.clone').find('div.reply_tip .lookTalk').css('display','none')
+    let spans = el.find('.clone').find('span')
+    spans[0].textContent = item.tousername ? `${item.username} 评论 ${item.tousername} :` : `${item.username} 评论 :`
+    spans[1].textContent = item.text
+    spans[2].textContent = item.time
+    spans[3].textContent = JSON.stringify([item.userid,item.username,item.tousername,item.commentid,item.id])
+    if (item.username === data.user) $('.clone').find('button.temp').text("删除").removeClass('btn-primary').addClass('btn-danger')
+    else $('.clone').find('button.temp').text("评论").removeClass('btn-danger').addClass('btn-primary')
+    el.find('.clone').clone().removeClass('clone').css('display','block').appendTo(el)
+  })
+  if(data.srcoll)  el.scrollTop(el.prop('scrollHeight'))
+  comment_comment_box.css('display','block')
+  setTimeout(() => {
+    comment_comment_box.addClass('comment_comment_box_in')
+    el.addClass('move_in')
+  }, 0)
+}
+
+
+function createLookTales(data, el) {
+  data.forEach((item) => {
+    let spans = el.find('.clone_lookTalk').find('span')
+    spans[0].textContent = item.tousername ? `${item.username} 评论 ${item.tousername} :` : `${item.username} 评论 :`
+    spans[1].textContent = item.text
+    spans[2].textContent = item.time
+    el.find('.clone_lookTalk').clone().removeClass('clone_lookTalk').css('display','block').appendTo(el)
+  })
+  setTimeout(() => {
+    el.parent().find('ul:first-child').removeClass('move_in')
+    el.addClass('lookTalk_container_in')
+    el.parent().find('.return').css('display',"block")
+  }, 0)
+  setTimeout(() => el.parent().find('.return').addClass('return_in'), 600)
+}
+
+
+
+$('.comment_comment_box').delegate('button.temp','click',function(){
+  if ($(this).text() === "评论") {
+    $(this).parent().css('display','none')
+    $(this).parent().next('.reply_text').css('display','block')
+  } else {
+    axios({
+      method:'delete',
+      url:`/delete/commentsComments/` + contentID + "/" + $(this).parents('.comment_box').find('.comments > span:first-child').text() + "/" + JSON.parse($(this).parent().next().find('span:last-child').text())[4],
+    }).then((res) => {
+        res_status(res.data, $(this))
+    })
+  }
+})
+
+$('.comment_comment_box').delegate('.reply_text button:nth-child(3)','click',function(){
+  $(this).parent().css('display','none')
+  $(this).parent().prev('.reply_tip').css('display','flow-root')
+})
+
+$('.comment_comment_box').delegate('.reply_text input','input',function(){
+  if ($(this).val().trim().length > 0) $(this).next().removeClass('notClick').addClass('btn-success').removeAttr('disabled')
+  else $(this).next().removeClass('btn-success').addClass('notClick').attr('disabled')
+})
+
+
+$('.comment_comment_box').delegate('.reply_text button:nth-child(2)','click',function(){
+  let text = $(this).prev().val()
+  let touser = JSON.parse($(this).next().next().text())
+  let commentid = $(this).parents('.comments').find('span:nth-child(1)')[0].textContent
+  axios({
+    method:'post',
+    url:'/replyComment',
+    data:{
+      text,
+      commentsCommentsId:touser[0],
+      tousername:touser[1] ,
+      commentid:commentid,
+      contentID,
+    }
+  }).then((res) => {
+      if (res.data.status === 108) $(this).next().click()
+      res_status(res.data, $(this).parents('.comment_comment_box'))
+  })
+})
+
+
+$('.comment_comment_box').delegate('.reply_tip .lookTalk','click',function(){
+  let data = JSON.parse($(this).parent().next().find('span').text())
+  let commentid = $(this).parents('.comments').find('span:nth-child(1)')[0].textContent
+  axios({
+    method:'get',
+    url:`/usersTalk/${contentID}/${data[3]}/${data[0]}/${data[2]}`,
+  }).then((res) => {
+      res_status(res.data, $(this))
+  })
+})
+
+function btn_return(el){
+  $(el).prev().removeClass('lookTalk_container_in')
+  $(el).removeClass('return_in')
+  $(el).parent().find('ul:first-child').addClass('move_in')
+  setTimeout(() => {
+     $(el).prev().find('li').not('.clone_lookTalk').remove()
+     $(el).css('display',"none")
+  },600)
+}
+
+
+$('.delete_content').click(function(){
+  axios({
+    method:'delete',
+    url:`/delete/content/` + contentID,
+  }).then((res) => {
+      res_status(res.data)
+  })
+})
+
+$('.delete_comment').click(function(){
+  axios({
+    method:'delete',
+    url:`/delete/comment/` + contentID + "/" + $(this).parents('.comment_box').find('.comments > span:first-child').text(),
+  }).then((res) => {
+      res_status(res.data, $(this))
+  })
+})
+
+
+$('.ico .ico-1').click(function(){
+  let status, target, url
+  if ($(this).hasClass('content-ico')) target = 1
+  else target = 2
+  if ($(this).prev()[0].checked) status = 2
+  else  status = 1
+  if (target === 1) url = `/greatNumber/${contentID}/${target}/${status}/_`
+  else url = `/greatNumber/${contentID}/${target}/${status}/${$(this).parents('.comment_box').find('.comments > span:first-child').text()}`
+  axios({
+    method:'put',
+    url,
+  }).then((res) => {
+      res.data.status_ = status
+      res_status(res.data, $(this))
+  })  
+})
---
+++ routes/index.js
@@ -0,0 +1,269 @@
+const express = require('express')
+const path = require('path')
+const svgCaptcha = require('svg-captcha')
+const router = express.Router()
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {      //登录中转
+  res.sendFile(path.join(__dirname, '../public/src', 'index.html'))
+})
+
+router.get('/user', async function(req, res, next) {      //登录中转
+  let datas = await db.all('SELECT * FROM contents ORDER BY id DESC')
+  if(req.session && req.session.login) {
+    res.json({user:req.user, datas})
+  }
+  else {
+    if (req.signedCookies.userID - 0 >= 0) {
+      let findUser = await db.get('SELECT id FROM users WHERE id = ?', req.signedCookies.userID - 0)
+      if (findUser) {
+        req.session.login = true
+        req.session.userID = findUser.id
+        res.json({user:req.user, datas})
+      } else {
+        res.clearCookie('userID')
+        res.json({user:null, datas})
+      }
+    } else res.json({user:null, datas})
+  }
+})
+
+
+router.get('/captcha', function(req, res, next) {   //发送验证码
+  let captcha = svgCaptcha.createMathExpr({noise:3, color:true, height:33.9, width:80,})
+  req.session.captcha = captcha.text.toUpperCase()
+  req.session.latsCaptchaTime = Date.now()
+  console.log(req.session.captcha)
+  console.log(req.session.latsCaptchaTime)
+  res.type('svg')
+  res.json({status:200, result:captcha.data, msg:"验证码"})
+})
+
+router.post('/registerORlogin', async (req, res, next) => {     //登录或注册
+  req.session.latsCaptchaTime = req.session.latsCaptchaTime || Date.now()
+  if (Date.now() - req.session.latsCaptchaTime > 180000)  {
+    console.log("验证码超时")
+    return res.json({status:205, type:req.body.type, msg:"验证码超时"})
+  }
+
+  if (req.body.captcha !== req.session.captcha) {
+    console.log("验证码错误")
+    return res.json({status:203, type:req.body.type, msg:"验证码错误"})
+  }
+
+  let cookieConfig = req.body.keepLogin 
+                    ? {maxAge: 60 * 1000 * 24 * 60 * 30, httpOnly: true, signed: true} 
+                    : {httpOnly: true, signed: true}
+
+  if (req.body.type === "register") {
+    let findUser = await db.get('SELECT name FROM users WHERE name = ?', req.body.username)
+    if (findUser) return res.json({status:201, type:req.body.type, msg:"用户名已存在"})
+    else {
+        db.run('INSERT INTO users (name, password, avatarPath) VALUES (?,?,?)', 
+        req.body.username, 
+        req.body.password,
+        req.body.avatar)
+      let user = await db.get('SELECT * FROM users WHERE name = ?', req.body.username)
+      res.cookie('userID', user.id, cookieConfig)
+      req.session.login = true
+      req.session.userID = user.id
+      res.json({status:100, type:req.body.type, msg:"注册成功"})
+    }
+  }
+
+  if (req.body.type === "login") {    //登录
+    let findUser = await db.get('SELECT name FROM users WHERE name = ?', req.body.username)
+    if (!findUser) return res.json({status:201, type:req.body.type, msg:"用户名不存在"})
+    else {
+      let user = await db.get('SELECT * FROM users WHERE name = ?', req.body.username)
+      if (user.password !== req.body.password) return res.json({status:202, type:req.body.type, msg:"密码错误"})
+      res.cookie('userID', user.id, cookieConfig)
+      req.session.login = true
+      req.session.userID = user.id
+      res.json({status:100, type:req.body.type, msg:"登陆成功"})
+    }
+  }
+
+})
+
+router.get('/logOut', (req, res, next) => {  //退出登录
+  req.session.login = false
+  res.clearCookie('userID')
+  res.redirect('/')
+})
+
+
+router.post('/add_post', async (req, res, next) => {     //发表文章
+  console.log(req.body.content)
+  if (req.user && req.user.id - 0 >= 0) {
+    db.run('INSERT INTO contents (title, content, time, userid, username, greatNumber, browseNumber) VALUES (?,?,?,?,?,?,?)', 
+           req.body.title, req.body.content, new Date().toLocaleString(), req.user.id - 0, req.user.name, 0, 0)
+    let contentID = await db.get('SELECT id FROM contents WHERE  username=? ORDER BY id DESC LIMIT 1', req.user.name)
+    res.json({status:100, msg:"发帖成功", userID: req.user.id, contentID:contentID.id})
+  } else user_error(req, res)
+
+})
+
+
+
+router.get('/content/:contentID', async (req, res, next) => {   //获取文章详情页
+  if (req.user && req.user.id - 0 >= 0) {
+    let contentData = await db.get('SELECT * FROM contents WHERE id=?', req.params.contentID - 0)
+    if (!contentData) res.redirect('/')
+    let contentGreat
+    let datas = await Promise.all([db.all('SELECT * FROM comments WHERE contentid=? ORDER BY id ASC', req.params.contentID - 0),
+                                   db.get('SELECT * FROM users WHERE id=?', req.user.id - 0),
+                                   db.get('SELECT browsingHistory FROM users WHERE id=?', req.user.id)])
+    let commentData = datas[0]
+    let commentsGreat = datas[1].greatCommentsHistory
+    let userBrows = datas[2]
+    if (!userBrows.browsingHistory || userBrows.browsingHistory.indexOf(`[${req.params.contentID}]`) < 0) {
+      userBrows = `[${req.params.contentID}]` + (userBrows.browsingHistory ? userBrows.browsingHistory : "")
+      db.run('UPDATE users SET browsingHistory = ? WHERE id = ?', userBrows, req.user.id - 0)
+      db.run('UPDATE contents SET browseNumber = browseNumber + 1 WHERE id = ?', req.params.contentID)
+    }
+    if (datas[1].greatHistory && datas[1].greatHistory.indexOf(`[${req.params.contentID}]`) >= 0) contentGreat = true
+    commentData.forEach((item) => {
+      if (commentsGreat && commentsGreat.indexOf(`[${item.id}]`) >= 0) item.great = true
+    })
+    if (contentData.username === req.user.name) contentData.isContentUser = true
+    res.render('content',{contentGreat, user:req.user, contentData, commentData})
+  } else user_error(req, res)
+})
+
+
+router.post('/add_comment', async (req, res, next) => {     //提交评论
+  console.log(req.body.text)
+  if (req.user && req.user.id - 0 >= 0) {
+    db.run('INSERT INTO comments (comment, contentid, userid, username, time, sumComments, greatNumber) VALUES (?,?,?,?,?,?,?)', 
+           req.body.text, req.body.contentID - 0, req.user.id - 0, req.user.name, new Date().toLocaleString(), 0, 0)
+    res.json({status:100, msg:"评论成功", contentID:req.body.contentID})
+  } else user_error(req, res)
+})
+
+
+router.post('/add_commentsComments', async (req, res, next) => {     //提交评论的评论
+  console.log(req.body.text)
+  let time = new Date().toLocaleString()
+  let isCommentuser
+  if (req.user && req.user.id - 0 >= 0) {
+    db.run('UPDATE comments SET sumComments = sumComments + 1 WHERE id = ?', req.body.commentid - 0)
+    db.run('INSERT INTO commentsComments (userid, username, commentid, time, text, contentid) VALUES (?,?,?,?,?,?)', 
+           req.user.id - 0, req.user.name, req.body.commentid - 0, time, req.body.text, req.body.contentID - 0)
+    if (req.body.tousername === req.user.name) isCommentuser = true
+    res.json({status:101, msg:"评论成功", text:req.body.text, time, 
+              isCommentuser, username:req.user.name, commentid:req.body.commnetid})
+  } else user_error(req, res)
+})
+
+router.get('/commentsComments/:commentID', async (req, res, next) => {     //获取评论的评论
+  if (req.user && req.user.id - 0 >= 0) {
+    let commentCommentsData = await db.all('SELECT * FROM commentsComments WHERE commentid=? ORDER BY id ASC', req.params.commentID - 0)
+    res.json({status:102, user:req.user.name, commentCommentsData, msg:"拉取成功"})  
+  } else user_error(req, res)
+})
+
+
+router.post('/replyComment', async (req, res, next) => {     //提交评论的评论的评论  哈哈哈哈
+  console.log(req.body.text)
+  let time = new Date().toLocaleString()
+  if (req.user && req.user.id - 0 >= 0) {
+    db.run('UPDATE comments SET sumComments = sumComments + 1 WHERE id = ?', req.body.commentid - 0)
+    await db.run('INSERT INTO commentsComments (userid, username, commentsCommentsId, commentid, tousername, time, text, contentid) VALUES (?,?,?,?,?,?,?,?)', 
+           req.user.id - 0, req.user.name, req.body.commentsCommentsId - 0, req.body.commentid - 0, req.body.tousername, time, req.body.text, req.body.contentID - 0)
+    if (req.body.tousername === req.user.name) isCommentuser = true
+    let commentCommentsData = await db.get('SELECT * FROM commentsComments WHERE userid =? AND time =?', req.user.id - 0, time)
+    res.json({status:108, msg:"评论成功", user:req.user.name, commentCommentsData})
+  } else user_error(req, res)
+})
+
+
+router.get('/usersTalk/:contentID/:commentid/:lookUserID/:tousername', async (req, res, next) => {     //获取对话评论
+  if (req.user && req.user.id - 0 >= 0) {
+    let data = await db.all('SELECT * FROM commentsComments WHERE contentid=? AND commentid=? AND (userid=? OR username=?) ORDER BY id ASC', 
+      req.params.contentID - 0, req.params.commentid - 0, req.params.lookUserID - 0, decodeURI(req.params.tousername))
+    res.json({status:109, data, msg:"拉取成功"})  
+  } else user_error(req, res)
+})
+
+
+router.delete('/delete/content/:contentid', async (req, res, next) => {     //删除文章
+  if (req.user && req.user.id - 0 >= 0) {
+    let contentUser = await db.get('SELECT * FROM contents WHERE id=?', req.params.contentid - 0)
+    if (req.user.id === contentUser.userid && req.user.name === contentUser.username) {
+      db.run('DELETE FROM contents WHERE id = ?', req.params.contentid - 0)
+      db.run('DELETE FROM comments WHERE contentid = ?', req.params.contentid - 0)
+      db.run('DELETE FROM commentsComments WHERE contentid = ?', req.params.contentid - 0)
+      res.json({status:50, msg:"删除成功"})  
+    } else user_error(req, res)
+  } else user_error(req, res)
+})
+
+
+
+
+router.delete('/delete/comment/:contentid/:commentid', async (req, res, next) => {     //删除文章评论
+  if (req.user && req.user.id - 0 >= 0) {
+    let commentUser = await db.get('SELECT * FROM comments WHERE id=?', req.params.commentid - 0)
+    if (req.user.id === commentUser.userid && req.user.name === commentUser.username) {
+      db.run('DELETE FROM comments WHERE id = ?', req.params.commentid - 0)
+      db.run('DELETE FROM commentsComments WHERE commentid = ?', req.params.commentid - 0)
+      res.json({status:51, msg:"删除成功"})  
+    } else user_error(req, res)
+  } else user_error(req, res)
+})
+
+
+router.delete('/delete/commentsComments/:contentid/:commentid/:commentsCommentsid', async (req, res, next) => {     //删除文章评论的评论
+  if (req.user && req.user.id - 0 >= 0) {
+    let commentsCommentsUser = await db.get('SELECT * FROM commentsComments WHERE id=?', req.params.commentsCommentsid - 0)
+    if (req.user.id === commentsCommentsUser.userid && req.user.name === commentsCommentsUser.username) {
+      db.run('DELETE FROM commentsComments WHERE id = ?', req.params.commentsCommentsid - 0)
+      db.run('UPDATE comments SET sumComments = sumComments - 1 WHERE id = ?', req.params.commentid - 0)
+      res.json({status:52, msg:"删除成功"})  
+    } else user_error(req, res)
+  } else user_error(req, res)
+})
+
+router.put('/greatNumber/:contentid/:target/:status/:commentid',  async (req, res, next) => {     //更新点赞数据
+  if (req.user && req.user.id - 0 >= 0) {
+    let userContentGreat, userCommentGreat
+    if (req.params.status - 0 === 1) {
+      if (req.params.target - 0 === 1) {
+        db.run('UPDATE contents SET greatNumber = greatNumber + 1 WHERE id = ?', req.params.contentid - 0)
+        userContentGreat = await db.get('SELECT greatHistory FROM users WHERE id=?', req.user.id)
+        userContentGreat = `[${req.params.contentid}]` + (userContentGreat.greatHistory ? userContentGreat.greatHistory : "")
+        db.run('UPDATE users SET greatHistory = ? WHERE id = ?', userContentGreat, req.user.id - 0)
+      }
+      else {
+        db.run('UPDATE comments SET greatNumber = greatNumber + 1 WHERE id = ?', req.params.commentid - 0)
+        userCommentGreat = await db.get('SELECT greatCommentsHistory FROM users WHERE id=?', req.user.id - 0)
+        userCommentGreat = `[${req.params.commentid}]` + (userCommentGreat.greatCommentsHistory ? userCommentGreat.greatCommentsHistory : "")
+        db.run('UPDATE users SET greatCommentsHistory = ? WHERE id = ?', userCommentGreat, req.user.id - 0)
+      }
+    } else {
+      if (req.params.target - 0 === 1) {
+        db.run('UPDATE contents SET greatNumber = greatNumber - 1 WHERE id = ?', req.params.contentid - 0)
+        userContentGreat = await db.get('SELECT greatHistory FROM users WHERE id=?', req.user.id)
+        userContentGreat = userContentGreat.greatHistory.replace(`[${req.params.contentid}]`, "")
+        db.run('UPDATE users SET greatHistory = ? WHERE id = ?', userContentGreat, req.user.id - 0)
+      }
+      else {
+        db.run('UPDATE comments SET greatNumber = greatNumber - 1 WHERE id = ?', req.params.commentid - 0)
+        userCommentGreat = await db.get('SELECT greatCommentsHistory FROM users WHERE id=?', req.user.id - 0)
+        userCommentGreat = userCommentGreat.greatCommentsHistory.replace(`[${req.params.commentid}]`, "")
+        db.run('UPDATE users SET greatCommentsHistory = ? WHERE id = ?', userCommentGreat, req.user.id - 0)
+      }
+    }
+    res.json({status:53, msg:"点赞(取消点赞)"})  
+  } else user_error(req, res)
+})
+
+
+function user_error(req, res){
+  req.session.login = false
+  res.clearCookie('userID')
+  res.redirect('/')
+}
+
+module.exports = router
